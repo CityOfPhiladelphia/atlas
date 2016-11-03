c7aa1b522bd1b92f13136953ea9c42bf537b2900
Robert Martin <rmartin@rmart.in>
2016-11-03 18:32:09 +0000

    Factor map out of main.js

--- js/main.js
+++ js/main.js
@@ -75,51 +75,6 @@ var app = (function ()
     {
       DEBUG && $('#search-input').val(DEBUG_ADDRESS);
       
-      var CITY_HALL = [39.952388, -75.163596];
-      map = L.map('map', {
-         zoomControl: false,
-      //   measureControl: true,
-      });
-      map.setView(CITY_HALL, 16);
-      
-      // Basemaps
-      var baseMapLight = L.esri.tiledMapLayer({
-        url: "https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap/MapServer"
-        // url: "https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_2015_3in/MapServer"
-      });
-      var baseMapDark = L.esri.tiledMapLayer({
-        url: "https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityBasemap_Slate/MapServer"
-      });
-      var baseMapImagery2015 = L.esri.tiledMapLayer({
-        url: "https://tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_2015_3in/MapServer"
-      });
-      baseMapLight.addTo(map);
-      
-      // Overlays
-      var overlayZoning = L.esri.tiledMapLayer({
-        url: '//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/ZoningMap_tiled/MapServer'
-      });
-      var overlayPwdParcels = L.esri.featureLayer({
-        url: '//services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/PWD_PARCELS/FeatureServer/0'
-      });
-      
-      var baseLayers = {
-        'Light':    baseMapLight,
-        'Dark':     baseMapDark,
-        'Imagery 2015': baseMapImagery2015,
-      };
-      var overlays = {
-        'Zoning':     overlayZoning,
-        'PWD Parcels':  overlayPwdParcels,
-        // 'Land Use': landUse,
-      };
-      
-      // Controls
-      L.control.layers(baseLayers, overlays, {position: 'topright'}).addTo(map);
-      var measureControl = new L.Control.Measure({position: 'topright'});
-      measureControl.addTo(map);
-      new L.Control.Zoom({position: 'topright'}).addTo(map);
-      
       // TEMP: just for mockup. Listen for clicks on data row link.
       $('.data-row-link').click(function (e) {
         e.preventDefault();
