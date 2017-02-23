d8229d8513a10142741606e60e8ba44a89ac7652
Robert Martin <rmartin@rmart.in>
2017-02-23 17:57:40 -0500

    Update push_config.bat

--- scripts/push_config.bat
+++ scripts/push_config.bat
@@ -1,7 +1,7 @@
 echo off
 
 rem     Push the js/config.js file to an S3 bucket. Takes an argument for which
-rem     bucket to use (dev or prod).
+rem     bucket to use (dev or prod). Requires the AWS CLI to be installed.
 rem     Usage: push_config [dev|prod]
 
 set env=%1
