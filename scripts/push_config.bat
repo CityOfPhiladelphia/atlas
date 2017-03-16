c036cce64bffb67be38a29baec087d2571096abc
andy.rothwell <rothwell.andy@gmail.com>
2017-03-16 15:21:12 -0400

    changes to bat for pushing prod and dev config

--- scripts/push_config.bat
+++ scripts/push_config.bat
@@ -4,9 +4,19 @@ rem     Push the js/config.js file to an S3 bucket. Takes an argument for which
 rem     bucket to use (dev or prod). Requires the AWS CLI to be installed.
 rem     Usage: push_config [dev|prod]
 
-set env=%1
-rem     set dest=s3://phila-atlas-%env%/js/config.js
-IF "%env%"=="prod" (set dest=s3://atlas.phila.gov/js/config.js)
-ELSE (set dest=s3://atlas-%env%.phila.gov/js/config.js)
-echo Pushing...
-aws s3 cp ..\js\config.js %dest%
+set dest=
+
+IF %1==prod (
+  echo Pushing To Prod...
+  set dest=s3://atlas.phila.gov/js/config.js
+)
+IF %1==dev (
+  echo Pushing To Dev...
+  set dest=s3://atlas-%env%.phila.gov/js/config.js
+)
+IF EXIST %dest% (
+  aws s3 cp ..\js\config.js %dest%
+) ELSE (
+  echo Must Provide Environment
+  exit 1
+)
