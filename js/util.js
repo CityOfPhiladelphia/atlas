014c291e34c4f202f4afa1a83a8d69cb813a8638
Robert Martin <rmartin@rmart.in>
2017-05-09 14:15:14 -0400

    Add RCOs to Zoning tab

--- js/util.js
+++ js/util.js
@@ -318,5 +318,11 @@ app.util = (function () {
     numberWithCommas: function (x) {
       return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     },
+
+    formatPhoneNumber: function (s) {
+      var s2 = (""+s).replace(/\D/g, '');
+      var m = s2.match(/^(\d{3})(\d{3})(\d{4})$/);
+      return (!m) ? null : "(" + m[1] + ") " + m[2] + "-" + m[3];
+    },
   };
 }());
