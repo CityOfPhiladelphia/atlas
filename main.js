613eca072f3519ba0d79a46ad7582fe01939851d
Robert Martin <rmartin@rmart.in>
2017-10-20 14:09:16 -0400

    Disable data source `liPermitsAdditional`
    This is hosted internally so it won't work for public Atlas.

--- main.js
+++ main.js
@@ -254,22 +254,22 @@ Mapboard.default({
         }
       }
     },
-    liPermitsAdditional: {
-      type: 'http-get',
-      url: 'http://ase.phila.gov/arcgis/rest/services/GSG/GIS_LNI_LI_PERMITS_PLANNING/FeatureServer/0/query?',
-      options: {
-        params: {
-          where: function(feature) {
-            return "ADDRESS = '" + feature.properties.street_address + "' OR ADDRESSKEY = '" + feature.properties.li_address_key + "'"
-          },
-          outFields: '*',
-          f: 'json'
-        },
-        success: function(data) {
-          return data.features;
-        },
-      },
-    },
+    // liPermitsAdditional: {
+    //   type: 'http-get',
+    //   url: 'http://ase.phila.gov/arcgis/rest/services/GSG/GIS_LNI_LI_PERMITS_PLANNING/FeatureServer/0/query?',
+    //   options: {
+    //     params: {
+    //       where: function(feature) {
+    //         return "ADDRESS = '" + feature.properties.street_address + "' OR ADDRESSKEY = '" + feature.properties.li_address_key + "'"
+    //       },
+    //       outFields: '*',
+    //       f: 'json'
+    //     },
+    //     success: function(data) {
+    //       return data.features;
+    //     },
+    //   },
+    // },
     liInspections: {
       type: 'http-get',
       url: 'https://phl.carto.com/api/v2/sql',
