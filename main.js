91cd82302b7d3fed07182628a999e4199bf1288f
Robert Martin <rmartin@rmart.in>
2017-11-09 18:39:11 -0500

    Fix nearby text filter label

--- main.js
+++ main.js
@@ -2444,7 +2444,7 @@ Mapboard.default({
                     }
                   ],
                   filterByText: {
-                    label: 'Filter by',
+                    label: 'Filter by text',
                     fields: [
                       'service_name',
                       'address'
