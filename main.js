7d9c65468720d8313a094f48cf3c4052824b86a9
Robert Martin <rmartin@rmart.in>
2017-11-13 10:13:47 -0500

    Update Condomniums tab to match changes made to CityAtlas
    * New topic key ('condominiums')
    * Removed external icon from links
    * Removed link from OPA account (it goes to the same place as address)
    * Swapped address and OPA account columns

--- main.js
+++ main.js
@@ -1134,25 +1134,22 @@ Mapboard.default({
         {
           type: 'horizontal-table',
           options: {
-            topicKey: 'condos',
+            topicKey: 'condominiums',
             id: 'condoList',
             useApiCount: true,
             defaultIncrement: 25,
             fields: [
               {
-                label: 'OPA Account',
-                value: function(state, item) {
-                  var url = window.location.origin + window.location.pathname + '#/' + item.properties.opa_account_num + '/opa'
-                  return "<a href="+url+">"+item.properties.opa_account_num+" <i class='fa fa-external-link'></i></a>";
-                  // console.log('value function item:', item, 'controller:', controller);
-                  // return "<a onclick='" + controller + "'>"+item.properties.opa_account_num+"</a>"
+                label: 'Address',
+                value: function (state, item) {
+                  var address = item.properties.street_address;
+                  return '<a href="#/' + address + '/property">' + address + '</a>';
                 },
               },
               {
-                label: 'Address',
-                value: function(state, item) {
-                  var url = window.location.origin + window.location.pathname + '#/' + item.properties.opa_account_num + '/opa'
-                  return "<a href="+url+">"+item.properties.street_address+" <i class='fa fa-external-link'></i></a>";
+                label: 'OPA Account #',
+                value: function (state, item) {
+                  return item.properties.opa_account_num;
                 },
               },
             ], // end fields
