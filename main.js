6cf9ee4c141b294c4f38a1d983d089f59951cdcb
andy.rothwell <rothwell.andy@gmail.com>
2018-02-20 09:52:03 -0500

    updates dor baselayer labels

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ if (location.hostname !== 'localhost') {
 
 var GATEKEEPER_KEY = '82fe014b6575b8c38b44235580bc8b11';
 // var BASE_CONFIG_URL = '//raw.githubusercontent.com/rbrtmrtn/mapboard-base-config/develop/config.js';
-var BASE_CONFIG_URL = '//rawgit.com/rbrtmrtn/mapboard-base-config/c5c5b33fee4b375c0856f6b1215dc990b80f569c/config.js';
+var BASE_CONFIG_URL = '//rawgit.com/rbrtmrtn/mapboard-base-config/e45803b240e14717fb452805fa90c134870eb14b/config.js';
 
 var ZONING_CODE_MAP = {
   'RSD-1': 'Residential Single Family Detached-1',
