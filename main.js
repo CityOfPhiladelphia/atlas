52444076b387e7b0ebd53aed9425ea2a5267cb81
andy.rothwell <rothwell.andy@gmail.com>
2018-02-23 16:24:33 -0500

    uses mapboard 1.2.32 - adds new topics Polling and RCO Notification

--- main.js
+++ main.js
@@ -227,27 +227,44 @@ Mapboard.default({
         }
       }
     },
-    // TODO elections and divisions
-    // elections: {
-    //   url: 'https://api.phila.gov/elections',
-    //   type: 'http-get',
-    //   params: {
-    //
-    //   },
-    //   success(data) {
-    //     return data;
-    //   }
-    // }
-    // divisions: {
-    //   url: 'https://gis.phila.gov/arcgis/rest/services/PhilaGov/ServiceAreas/MapServer/22',
-    //   type: 'http-get',
-    //   params: {
-    //
-    //   },
-    //   success(data) {
-    //     return data;
-    //   }
-    // },
+    elections: {
+      url: 'http://api.phila.gov/pollingtest',
+      type: 'http-get',
+      options: {
+        params: {
+          option: 'com_pollingplaces',
+          view: 'json',
+          ward: function(feature) {
+            if (feature.properties.election_precinct !== '') {
+              return feature.properties.election_precinct.slice(0,2);
+            } else {
+              return feature.properties.political_division.slice(0,2);
+            }
+          },
+          division: function(feature) {
+            if (feature.properties.election_precinct !== '') {
+              return feature.properties.election_precinct.slice(2,4);
+            } else {
+              return feature.properties.political_division.slice(2,4);
+            }
+          }
+        },
+        success(data) {
+          return data;
+        }
+      }
+    },
+    divisions: {
+      url: 'https://gis.phila.gov/arcgis/rest/services/PhilaGov/ServiceAreas/MapServer/22',
+      // type: 'http-get',
+      type: 'esri',
+      options: {
+        relationship: 'contains',
+      },
+      success: function(data) {
+        return data;
+      }
+    },
     // stormwater: {
     //   type: 'http-get',
     //   url: 'https://api.phila.gov/stormwater',
@@ -549,6 +566,17 @@ Mapboard.default({
       options: {
         geometryServerUrl: '//gis.phila.gov/arcgis/rest/services/Geometry/GeometryServer/',
         calculateDistance: true,
+        distances: 150,
+      },
+    },
+    neighboringProperties: {
+      type: 'esri-nearby',
+      url: 'https://services.arcgis.com/fLeGjb7u4uXqeF9q/ArcGIS/rest/services/PWD_PARCELS/FeatureServer/0',
+      // url: 'https://gis.phila.gov/arcgis/rest/services/Water/pv_data_geodb2/MapServer/0',
+      options: {
+        geometryServerUrl: '//gis.phila.gov/arcgis/rest/services/Geometry/GeometryServer/',
+        calculateDistance: true,
+        distances: 320,
       },
     },
     zoningOverlay: {
@@ -607,7 +635,7 @@ Mapboard.default({
             // loop over parts (whether it's simple or multipart)
             parts.forEach(function (coordPairs) {
               coordPairs.forEach(function (coordPair) {
-                console.log('coordPair', coordPair);
+                // console.log('coordPair', coordPair);
 
                 // if the polygon has a hole, it has another level of coord
                 // pairs, presumably one for the outer coords and another for
@@ -2235,6 +2263,237 @@ Mapboard.default({
       ],
       identifyFeature: 'dor-parcel',
       parcels: 'dor'
+    },
+    {
+      key: 'polling',
+      icon: 'book',
+      label: 'Polling',
+      dataSources: ['divisions', 'elections'],
+      components: [
+        {
+          type: 'callout',
+          slots: {
+            text: '\
+              Polling information for this address.\
+              The map faithfully reflects polling as described in \
+              recorded polling information.\
+              The polling boundaries displayed on the map are for polling \
+              Source: Department of Polling\
+            ',
+          }
+        },
+        {
+          type: 'vertical-table',
+          slots: {
+            fields: [
+              {
+                label: 'Ward',
+                value: function(state) {
+                  if (state.sources.elections.data.features[0]) {
+                    return state.sources.elections.data.features[0].attributes.ward;
+                  }
+                  // return state.geocode.data.properties.political_ward;
+                }
+              },
+              {
+                label: 'Division',
+                value: function(state) {
+                  if (state.sources.elections.data.features[0]) {
+                    return state.sources.elections.data.features[0].attributes.division;
+                  }
+                  // return state.geocode.data.properties.political_division;
+                }
+              },
+              {
+                label: 'Polling Location',
+                value: function(state) {
+                  if (state.sources.elections.data.features[0]) {
+                    return state.sources.elections.data.features[0].attributes.location;
+                  }
+                }
+              },
+              {
+                label: 'Polling Address',
+                value: function(state) {
+                  if (state.sources.elections.data.features[0]) {
+                    return state.sources.elections.data.features[0].attributes.display_address;
+                  }
+                }
+              }
+            ]
+          }
+        }
+      ],
+      zoomToShape: ['geojson', 'marker'],
+      geojson: {
+        path: ['divisions', 'data'],
+        key: 'id',
+        style: {
+          fillColor: '#42f459',
+          color: '#42f459',
+          weight: 2,
+          opacity: 1,
+          fillOpacity: 0.3
+        }
+      },
+      marker: {
+        path: ['elections', 'data', 'features', '0', 'attributes'],
+        lat: 'lat',
+        lng: 'lng',
+        key: 'display_address',
+        color: '#42f459'
+      },
+      basemap: 'pwd',
+      identifyFeature: 'address-marker',
+      parcels: 'pwd'
+    },
+    {
+      key: 'rco',
+      icon: 'map-marker',
+      label: 'RCO Notification',
+      dataSources: ['neighboringProperties'],
+      components: [
+        {
+          type: 'callout',
+          slots: {
+            text: '\
+              These are your neighbors that you have to inform\
+              if you are going to try to change your zoning...\
+            ',
+          }
+        },
+        {
+          type: 'horizontal-table',
+          options: {
+            topicKey: 'rco',
+            id: 'neighboringProperties',
+            downloadButton: true,
+            downloadFile: 'neighboring-properties',
+            // sort: {
+            //   select: true,
+            //   sortFields: [
+            //     'date',
+            //     'distance'
+            //   ],
+            //   getValue: function(item, sortField) {
+            //     var val;
+            //     if (sortField === 'date' || !sortField) {
+            //       val = item.requested_datetime;
+            //     } else if (sortField === 'distance') {
+            //       val = item.distance;
+            //     }
+            //     return val;
+            //   },
+            //   order: function(sortField) {
+            //     var val;
+            //     if (sortField === 'date') {
+            //       val = 'desc';
+            //     } else {
+            //       val = 'asc';
+            //     }
+            //     return val;
+            //   }
+            // },
+            // filters: [
+            //   {
+            //     type: 'time',
+            //     getValue: function(item) {
+            //       return item.requested_datetime;
+            //     },
+            //     label: 'From the last',
+            //     values: [
+            //       {
+            //         label: '30 days',
+            //         value: '30',
+            //         unit: 'days',
+            //         direction: 'subtract',
+            //       },
+            //       {
+            //         label: '90 days',
+            //         value: '90',
+            //         unit: 'days',
+            //         direction: 'subtract',
+            //       },
+            //       {
+            //         label: 'year',
+            //         value: '1',
+            //         unit: 'years',
+            //         direction: 'subtract',
+            //       }
+            //     ]
+            //   }
+            // ],
+            // filterByText: {
+            //   label: 'Filter by text',
+            //   fields: [
+            //     'service_name',
+            //     'address'
+            //   ]
+            // },
+            mapOverlay: {
+              marker: 'geojson',
+              style: {
+                // radius: 6,
+                fillColor: 'red',
+                // fillColor: '#ff3f3f',
+                color: 'red',
+                weight: 2,
+                opacity: 1,
+                fillOpacity: 0.3
+              },
+              hoverStyle: {
+                // radius: 6,
+                fillColor: 'yellow',
+                color: '#ff0000',
+                weight: 2,
+                opacity: 1,
+                fillOpacity: 0.3
+              }
+            },
+            fields: [
+              {
+                label: 'Address',
+                value: function(state, item) {
+                  return item.properties.ADDRESS;
+                },
+              },
+              // {
+              //   label: 'Description',
+              //   value: function(state, item) {
+              //     return item.properties.BLDG_DESC;
+              //   }
+              // },
+              {
+                label: 'Distance',
+                value: function(state, item) {
+                  return parseInt(item._distance) + ' ft';
+                }
+              }
+            ]
+          },
+          slots: {
+            title: 'Neighboring Properties',
+            data: 'neighboringProperties',
+            items: function(state) {
+              var data = state.sources['neighboringProperties'].data || [];
+              var rows = data.map(function(row){
+                var itemRow = row;
+                // var itemRow = Object.assign({}, row);
+                return itemRow;
+              });
+              return rows;
+            },
+          }
+        },
+      ],
+      // geojson: {
+      //   path: ['neighboringProperties', 'data'],
+      //   color: 'red',
+      //   key: 'id'
+      // },
+      basemap: 'pwd',
+      identifyFeature: 'address-marker',
+      parcels: 'pwd'
     },
     {
       key: 'nearby',
