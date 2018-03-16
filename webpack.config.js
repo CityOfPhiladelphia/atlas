022e71487fa5e53fa26d09240e5adbe9003aed4f
Robert Martin <rmartin@rmart.in>
2018-03-16 16:28:59 -0400

    Split vendor code into separate bundle [WIP]

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,55 @@
 const path = require('path');
 
+const env = process.env.NODE_ENV;
+const isDevelopment = env === 'development';
+
+console.log('NODE_ENV:', env);
+
 module.exports = {
-  entry: './src/main.js',
-  devtool: 'source-map',
+  // entry: './src/main.js',
+  entry: {
+    app: './src/main.js',
+    mapboard: '@cityofphiladelphia/mapboard',
+    // TODO figure out how to get this to resolve to the vendor chunk
+    vendor: 'vendor',
+  },
+  devtool: isDevelopment ? 'inline-source-map' : 'source-map',
   devServer: {
     contentBase: './public',
   },
   output: {
-    filename: 'app.js',
     path: path.resolve(__dirname, 'public'),
+    filename: '[name].js',
+    chunkFilename: '[name].js',
+    publicPath: '/',
+  },
+  devtool: isDevelopment ? 'inline-source-map' : 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'images/[name].[ext]?[hash]',
+        },
+      },
+    ],
+  },
+  mode: process.env.NODE_ENV,
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: 'initial',
+          name: 'vendor',
+          test: /[\\/]node_modules[\\/]/,
+          enforce: true,
+        },
+      },
+    },
   },
 };
