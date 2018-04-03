7c2311356961a910a8d7cdacf48a4f89afe8d1b2
andy.rothwell <rothwell.andy@gmail.com>
2018-04-03 16:03:42 -0400

    removes date from "Assessed Value" in Property Assessments table

--- main.js
+++ main.js
@@ -970,7 +970,7 @@ Mapboard.default({
                 }
               },
               {
-                label: 'Assessed Value ' + new Date().getFullYear(),
+                label: 'Assessed Value ',// + new Date().getFullYear(),
                 value: function(state) {
                   var data = state.sources.opa.data;
                   // return data.market_value;
