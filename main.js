92b9b467b4fc0ccd662f0fcf83fbcaddbe8af851
andy.rothwell <rothwell.andy@gmail.com>
2018-04-17 15:05:28 -0400

    uses mapboard 1.2.34 - makes zoning topic work with split-zoning

--- main.js
+++ main.js
@@ -2124,7 +2124,8 @@ Mapboard.default({
       icon: 'university',
       label: 'Zoning',
       dataSources: [
-        'zoningOverlay', 'zoningBase'
+        'zoningOverlay',
+        'zoningBase',
       ],
       components: [
         {
