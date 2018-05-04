6ae27243f6cf877502676a108f1679d5e30928d6
andy.rothwell <rothwell.andy@gmail.com>
2018-05-04 16:24:54 -0400

    uses mapboard 2.0.0-14 - replaces gatekeeper shim system

--- src/main.js
+++ src/main.js
@@ -26,52 +26,48 @@ if (location.hostname !== 'localhost') {
   console.log = console.info = console.debug = console.error = function () {};
 }
 
-var GATEKEEPER_KEY = '82fe014b6575b8c38b44235580bc8b11';
 
-// var BASE_CONFIG_URL = '//raw.githubusercontent.com/rbrtmrtn/mapboard-base-config/develop/config.js';
-// var BASE_CONFIG_URL = '//rawgit.com/rbrtmrtn/mapboard-base-config/e45803b240e14717fb452805fa90c134870eb14b/config.js';
-// var BASE_CONFIG_URL = 'https://cdn.rawgit.com/rbrtmrtn/mapboard-base-config/e45803b240e14717fb452805fa90c134870eb14b/config.js';
-var BASE_CONFIG_URL = 'https://cdn.rawgit.com/rbrtmrtn/mapboard-base-config/11f9644110fa1d6ff8a198f206d17631c8981947/config.js';
-
-var ZONING_CODE_MAP = {
-  'RSD-1': 'Residential Single Family Detached-1',
-  'RSD-2': 'Residential Single Family Detached-2',
-  'RSD-3': 'Residential Single Family Detached-3',
-  'RSA-1': 'Residential Single Family Attached-1',
-  'RSA-2': 'Residential Single Family Attached-2',
-  'RSA-3': 'Residential Single Family Attached-3',
-  'RSA-4': 'Residential Single Family Attached-4',
-  'RSA-5': 'Residential Single Family Attached-5',
-  'RTA-1': 'Residential Two-Family Attached-1',
-  'RM-1': 'Residential Multi-Family-1',
-  'RM-2': 'Residential Multi-Family-2',
-  'RM-3': 'Residential Multi-Family-3',
-  'RM-4': 'Residential Multi-Family-4',
-  'RMX-1': 'Residential Mixed-Use-1',
-  'RMX-2': 'Residential Mixed-Use-2',
-  'RMX-3': 'Residential (Center City) Mixed-Use-3',
-  'CA-1': 'Auto-Oriented Commercial-1',
-  'CA-2': 'Auto-Oriented Commercial-2',
-  'CMX-1': 'Neighborhood Commercial Mixed-Use-1',
-  'CMX-2': 'Neighborhood Commercial Mixed-Use-2',
-  'CMX-2.5': 'Neighborhood Commercial Mixed-Use-2.5',
-  'CMX-3': 'Community Commercial Mixed-Use',
-  'CMX-4': 'Center City Commercial Mixed-Use',
-  'CMX-5': 'Center City Core Commercial Mixed-Use',
-  'I-1': 'Light Industrial',
-  'I-2': 'Medium Industrial',
-  'I-3': 'Heavy Industrial',
-  'IP': 'Port Industrial',
-  'ICMX': 'Industrial Commercial Mixed-Use',
-  'IRMX': 'Industrial Residential Mixed-Use',
-  'SPENT': 'Commercial Entertainment (Casinos)',
-  'SPAIR': 'Airport',
-  'SPINS': 'Institutional Development',
-  'SPSTA': 'Stadium',
-  'SPPOA': 'Recreation',
-  'SP-PO-A': 'Recreation',
-  'SPPOP': 'Recreation',
-};
+var BASE_CONFIG_URL = 'https://cdn.rawgit.com/rbrtmrtn/mapboard-base-config/11f9644110fa1d6ff8a198f206d17631c8981947/config.js',
+    GATEKEEPER_KEY = '82fe014b6575b8c38b44235580bc8b11',
+    ZONING_CODE_MAP = {
+      'RSD-1': 'Residential Single Family Detached-1',
+      'RSD-2': 'Residential Single Family Detached-2',
+      'RSD-3': 'Residential Single Family Detached-3',
+      'RSA-1': 'Residential Single Family Attached-1',
+      'RSA-2': 'Residential Single Family Attached-2',
+      'RSA-3': 'Residential Single Family Attached-3',
+      'RSA-4': 'Residential Single Family Attached-4',
+      'RSA-5': 'Residential Single Family Attached-5',
+      'RTA-1': 'Residential Two-Family Attached-1',
+      'RM-1': 'Residential Multi-Family-1',
+      'RM-2': 'Residential Multi-Family-2',
+      'RM-3': 'Residential Multi-Family-3',
+      'RM-4': 'Residential Multi-Family-4',
+      'RMX-1': 'Residential Mixed-Use-1',
+      'RMX-2': 'Residential Mixed-Use-2',
+      'RMX-3': 'Residential (Center City) Mixed-Use-3',
+      'CA-1': 'Auto-Oriented Commercial-1',
+      'CA-2': 'Auto-Oriented Commercial-2',
+      'CMX-1': 'Neighborhood Commercial Mixed-Use-1',
+      'CMX-2': 'Neighborhood Commercial Mixed-Use-2',
+      'CMX-2.5': 'Neighborhood Commercial Mixed-Use-2.5',
+      'CMX-3': 'Community Commercial Mixed-Use',
+      'CMX-4': 'Center City Commercial Mixed-Use',
+      'CMX-5': 'Center City Core Commercial Mixed-Use',
+      'I-1': 'Light Industrial',
+      'I-2': 'Medium Industrial',
+      'I-3': 'Heavy Industrial',
+      'IP': 'Port Industrial',
+      'ICMX': 'Industrial Commercial Mixed-Use',
+      'IRMX': 'Industrial Residential Mixed-Use',
+      'SPENT': 'Commercial Entertainment (Casinos)',
+      'SPAIR': 'Airport',
+      'SPINS': 'Institutional Development',
+      'SPSTA': 'Stadium',
+      'SPPOA': 'Recreation',
+      'SP-PO-A': 'Recreation',
+      'SPPOP': 'Recreation',
+    };
 
 function cleanDorAttribute(attr) {
   // console.log('cleanDorAttribute is running with attr', attr);
@@ -173,6 +169,7 @@ mapboard({
     left: 0,
     right: 0,
   },
+  gatekeeperKey: GATEKEEPER_KEY,
   map: {
     // possibly should move to base config
     defaultBasemap: 'pwd',
