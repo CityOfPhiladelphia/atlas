885a788be9a4c3e5f6dec040f65baedaea64b231
andy.rothwell <rothwell.andy@gmail.com>
2018-08-25 16:38:35 -0400

    removes vestigial code from map.js

--- src/general/map.js
+++ src/general/map.js
@@ -5,19 +5,4 @@ export default {
   imagery: {
     enabled: true
   },
-  historicBasemaps: {
-    enabled: false
-  },
-  featureLayers: {
-    streetTrees: {
-      url: 'https://services.arcgis.com/fLeGjb7u4uXqeF9q/ArcGIS/rest/services/Philadelphia_Street_Trees/FeatureServer/0',
-      markerType: 'circleMarker',
-      color: 'green',
-      fillColor: 'green',
-      fillOpacity: 0.5,
-      weight: 1,
-      radius: 6,
-      minZoom: 19,
-    },
-  },
 }
