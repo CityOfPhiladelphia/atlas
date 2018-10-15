d17bbc33599fddabbeeb49a10d98847b2204a48b
CITY\Stephen.Skilton <stephen.skilton@hotmail.com>
2018-10-15 13:18:23 -0400

    Fixed findNearest to avoid error when state is not loaded

--- src/topics/trash.js
+++ src/topics/trash.js
@@ -1,11 +1,14 @@
 import transforms from '../general/transforms';
 
 const getNearest = function(state, field, distName) {
-
-  let min = Math.min.apply(null, state.sources[field].data.map(function(item) {return item[distName];}));
-  let result  = state.sources[field].data.filter(function(item){return item[distName] == min;} );
-  let nearest = result? result[0] : null;
-  return nearest
+  if(state) {
+    if(state.sources[field].data){
+      let min = Math.min.apply(null, state.sources[field].data.map(function(item) {return item[distName];}));
+      let result  = state.sources[field].data.filter(function(item){return item[distName] == min;} );
+      let nearest = result? result[0] : null;
+      return nearest
+    }
+  } else {return}
 };
 
 
