03d7f90abfaa2d6616bcfea6213165c9b22f0f3f
Andy Rothwell <rothwell.andy@gmail.com>
2018-10-26 15:17:20 -0400

    Update voting.js

--- src/topics/voting.js
+++ src/topics/voting.js
@@ -20,7 +20,6 @@ export default {
           href: function(state) {
             // var address = state.geocode.data.properties.street_address;
             // var addressEncoded = encodeURIComponent(address);
-            console.log("BALLOT!!!!" + state.sources.electedOfficials.data.rows[0]);
             return 'https://www.philadelphiavotes.com/ballot_paper/' + state.sources.electedOfficials.data.rows[0].ballot_file_id + '.pdf';
           },
         },
