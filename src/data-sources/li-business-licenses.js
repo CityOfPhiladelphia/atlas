d43f18dbe0f8d6916f1b34b71206b97ce36d71f5
andy.rothwell <rothwell.andy@gmail.com>
2018-11-02 11:42:30 -0400

    changes li business licenses to use eclipse_addressobjectid

--- src/data-sources/li-business-licenses.js
+++ src/data-sources/li-business-licenses.js
@@ -4,7 +4,8 @@ export default {
   url: 'https://phl.carto.com/api/v2/sql',
   options: {
     params: {
-      q: function(feature){ return "select * from li_business_licenses where street_address = '" + feature.properties.street_address + "'"},// + "' or addresskey = '" + feature.properties.li_address_key.toString() + "'",
+      // q: function(feature){ return "select * from li_business_licenses where street_address = '" + feature.properties.street_address + "'"},// + "' or addresskey = '" + feature.properties.li_address_key.toString() + "'",
+      q: function(feature){ return "select * from li_business_licenses where eclipse_addressobjectid = '" + feature.properties.eclipse_location_id + "'"},// + "' or addresskey = '" + feature.properties.li_address_key.toString() + "'",
     }
   }
 }
