855a561bb80c3cd4b1c209ead1f2d6debc300a0d
andy\Andy Rothwell <rothwell.andy@gmail.com>
2019-02-12 19:48:14 -0500

    uses vue cli3, and mapboard, comps, mapping, and datefetch that use vue cli3

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  presets: [
+    '@vue/app'
+  ],
+  // plugins: [
+  //   "dynamic-import-node-babel-7",
+  //   "@babel/plugin-syntax-dynamic-import"
+  // ]
+}
---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const Visualizer = require('webpack-visualizer-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  publicPath: '/atlas/',
+  configureWebpack: {
+    plugins: [
+      new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
+      new Visualizer({ filename: './statistics.html' })
+    ],
+  },
+  chainWebpack: (config) => {
+    config.plugins.delete('prefetch')
+  },
+  transpileDependencies: [
+    // can be string or regex
+    '@cityofphiladelphia/mapboard',
+    '@cityofphiladelphia/phila-vue-comps',
+    '@cityofphiladelphia/phila-vue-mapping',
+    // '@cityofphiladelphia/phila-vue-datafetch',
+    // /other-dep/
+  ]
+}
