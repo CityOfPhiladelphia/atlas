444dcd0601f3920121eca3e9f912ae20f7157d5b
stevetotheizz0 <stephen.skilton@phila.gov>
2019-03-11 12:32:11 -0400

    Updated new geometry server

--- src/data-sources/historic-nearby.js
+++ src/data-sources/historic-nearby.js
@@ -3,7 +3,7 @@ export default {
   type: 'esri-nearby',
   url: 'https://services.arcgis.com/fLeGjb7u4uXqeF9q/ArcGIS/rest/services/Historic_sites_PhilReg/FeatureServer/0',
   options: {
-    geometryServerUrl: '//gis.phila.gov/arcgis/rest/services/Geometry/GeometryServer/',
+    geometryServerUrl: 'https://gis-utils.databridge.phila.gov/arcgis/rest/services/Utilities/Geometry/GeometryServer',
     calculateDistance: true,
     distances: 660,
     limit: 10
