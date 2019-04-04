ca75011a560b5b7a0f1b4581067d09e2c9b5973c
andy.rothwell <rothwell.andy@gmail.com>
2019-04-04 12:25:03 -0400

    fixes business licenses return when there is no value

--- src/data-sources/li-business-licenses.js
+++ src/data-sources/li-business-licenses.js
@@ -5,6 +5,10 @@ export default {
   options: {
     params: {
       q: function(feature){
+        // if there is no eclipse_location_id, return a query that will definitely return nothing (or it will return many)
+        if (feature.properties.eclipse_location_id === null || feature.properties.eclipse_location_id === '') {
+          return "select * from li_business_licenses where eclipse_addressobjectid in (" + null + ")"
+        }
         var eclipseLocId = feature.properties.eclipse_location_id.split('|');
         var str = "'";
         var i;
@@ -13,6 +17,7 @@ export default {
           str += "', '"
         }
         str = str.slice(0, str.length - 3);
+
         return "select * from li_business_licenses where eclipse_addressobjectid in (" + str + ")"
       },
     }
