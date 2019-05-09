4fcd5dda06eb617bbd8ac5ca182928a51a75c137
andy.rothwell <rothwell.andy@gmail.com>
2019-05-09 16:19:41 -0400

    puts lat and lng into pollingplace data in carto query in polling-places.js, does a ton of filling out voting.js, because it didn't handle carto endpoint during data loading process

--- src/data-sources/polling-places.js
+++ src/data-sources/polling-places.js
@@ -4,7 +4,7 @@ id: 'pollingPlaces',
  url: 'https://phl.carto.com/api/v2/sql',
  options: {
    params: {
-     q: function(feature){ return "select * from polling_places where precinct = '" + feature.properties.election_precinct + "'"},
+     q: function(feature){ return "select ST_X(the_geom) as lng, ST_Y(the_geom) as lat, * from polling_places where precinct = '" + feature.properties.election_precinct + "'"},
    }
  }
 }
