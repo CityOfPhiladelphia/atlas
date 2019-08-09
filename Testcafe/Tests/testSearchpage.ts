f76130b78d7015b728f2db671b0ba9b647db055f
Anusha Errabelli <37459695+AnushaErrabelli@users.noreply.github.com>
2019-08-09 14:39:17 -0400

    updated Testsearchpage with more tests

--- Testcafe/Tests/testSearchpage.ts
+++ Testcafe/Tests/testSearchpage.ts
@@ -6,6 +6,12 @@ fixture`search page verification`.page `https://atlas.phila.gov/`;
 let searchpage = new Searchpage();
 
 test('search page verification', async (t: TestController) => {
-   
+
     await searchpage.verifySearchFunctionality(t);
+    await searchpage.verifytopicPropertyAssesmentFunctionality(t);
+    await searchpage.verifytopicDeedsFuctionality(t);
+    await searchpage.verifytopicLicensesandInspectionFuctionality(t)
+    await searchpage.verifyZoningFuctionality(t);
+    await searchpage.verifyVotingFuctionality(t);
+    await searchpage.verifyNearbyFuctionality(t);
 });
