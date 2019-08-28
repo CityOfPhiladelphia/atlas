3abe33379d00b9cd8da0f137943668a7495b329b
andy.rothwell <rothwell.andy@gmail.com>
2019-08-28 12:26:46 -0400

    finishes copying dor docs query to carto

--- src/data-sources/dor-documents.js
+++ src/data-sources/dor-documents.js
@@ -18,17 +18,61 @@ export default {
       q: function(feature, state){
         var parcelBaseAddress = helpers.concatDorAddress(feature);
         var geocode = state.geocode.data.properties;
-        // if (!parcelBaseAddress || parcelBaseAddress === 'null'){
-        //   var where = "MATCHED_REGMAP = '" + state.parcels.dor.data[0].properties.BASEREG + "'";
-        // } else {
+
+        console.log('parcelBaseAddress:', parcelBaseAddress);
+
+        var query = "select * from rtt_summary WHERE "
+        if (!parcelBaseAddress || parcelBaseAddress === 'null'){
+          query += "MATCHED_REGMAP = '" + state.parcels.dor.data[0].properties.BASEREG + "'";
+        } else {
           // TODO make these all camel case
-        var props = state.geocode.data.properties,
-            address_low = props.address_low,
-            address_floor = Math.floor(address_low / 100, 1) * 100,
-            address_remainder = address_low - address_floor,
-            addressHigh = props.address_high,
-            addressCeil = addressHigh || address_low;
-        return "select * from rtt_summary WHERE (((ADDRESS_LOW >= " + address_low + " AND ADDRESS_LOW <= " + address_low + ") OR (ADDRESS_LOW >= " + address_floor + " AND ADDRESS_LOW <= " + address_low + " AND CAST (ADDRESS_HIGH AS numeric) >= " + address_remainder + ")) AND STREET_NAME = '" + geocode.street_name + "' AND STREET_SUFFIX = '" + geocode.street_suffix + "' AND (MOD(ADDRESS_LOW,2) =MOD( " + address_low + ",2))) OR MATCHED_REGMAP = '" + state.parcels.dor.data[0].properties.BASEREG + "' OR REG_MAP_ID = '" + state.parcels.dor.data[0].properties.BASEREG + "'"
+          var props = state.geocode.data.properties,
+              address_low = props.address_low,
+              address_floor = Math.floor(address_low / 100, 1) * 100,
+              address_remainder = address_low - address_floor,
+              addressHigh = props.address_high,
+              addressCeil = addressHigh || address_low;
+
+          query += "(((ADDRESS_LOW >= " + address_low
+                + " AND ADDRESS_LOW <= " + address_low + ") OR (ADDRESS_LOW >= " + address_floor
+                + " AND ADDRESS_LOW <= " + address_low
+                + " AND CAST (ADDRESS_HIGH AS numeric) >= " + address_remainder
+                + ")) AND STREET_NAME = '" + geocode.street_name
+                + "' AND STREET_SUFFIX = '" + geocode.street_suffix
+                + "' AND (MOD(ADDRESS_LOW,2) = MOD(" + address_low + ",2)))";
+
+          if (geocode.street_predir != '') {
+            query += " AND STREET_PREDIR = '" + geocode.street_predir + "'";
+          }
+
+          if (geocode.address_low_suffix != '') {
+            query += " AND ADDRESS_LOW_SUFFIX = '" + geocode.address_low_suffix + "'";
+          }
+
+          // this is hardcoded right now to handle DOR address suffixes that are actually fractions
+          if (geocode.address_low_frac === '1/2') {
+            query += " AND ADDRESS_LOW_SUFFIX = '2'" //+ geocode.address_low_frac + "'";
+          }
+
+          if (geocode.street_postdir != '') {
+            query += " AND STREET_POSTDIR = '" + geocode.street_postdir + "'";
+          }
+
+          // check for unit num
+          var unitNum = helpers.cleanDorAttribute(feature.properties.UNIT),
+              unitNum2 = geocode.unit_num;
+
+          if (unitNum) {
+            query += " AND UNIT_NUM = '" + unitNum + "'";
+          } else if (unitNum2 !== '') {
+            query += " AND UNIT_NUM = '" + unitNum2 + "'";
+          }
+
+          query += "OR MATCHED_REGMAP = '" + state.parcels.dor.data[0].properties.BASEREG
+                + "' OR REG_MAP_ID = '"+ state.parcels.dor.data[0].properties.BASEREG + "'";
+
+        }
+        return query;
       }
     }
   },
