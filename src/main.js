f9c6e25f7014c73ed79b21a1c13f44667d45ea83
andy.rothwell <rothwell.andy@gmail.com>
2019-12-06 15:17:30 -0500

    changes to test travis

--- src/main.js
+++ src/main.js
@@ -10,8 +10,6 @@
 // import * as Sentry from '@sentry/browser';
 // Sentry.init({ dsn: 'https://276ef359f45543ff91b7db449d3035f8@sentry.io/1330874' });
 
-console.log('atlas at top of main.js');
-
 // turn off console logging in production
 const { hostname='' } = location;
 if (hostname !== 'localhost' && !hostname.match(/(\d+\.){3}\d+/)) {
