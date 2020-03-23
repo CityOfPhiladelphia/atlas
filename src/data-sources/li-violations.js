fbab822931227fb7ff5262b378b02f76061a2b74
Andy Rothwell <rothwell.andy@gmail.com>
2020-03-23 11:13:38 -0400

    uses new violations table, search it by opa_account_num

--- src/data-sources/li-violations.js
+++ src/data-sources/li-violations.js
@@ -5,7 +5,8 @@ export default {
   options: {
     params: {
       q: function(feature){
-        return "select * from li_violations where address = '" + feature.properties.street_address + "' or addresskey = '" + feature.properties.li_address_key.toString() + "'";
+        // return "select * from li_violations where address = '" + feature.properties.street_address + "' or addresskey = '" + feature.properties.li_address_key.toString() + "'";
+        return "select * from violations where opa_account_num = '" + feature.properties.opa_account_num + "'";
       },
     },
   },
