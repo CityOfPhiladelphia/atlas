8283f4a99ae4bfbce77fc25aab3d6f39773d149f
Andy Rothwell <rothwell.andy@gmail.com>
2020-03-23 11:00:11 -0400

    correctly uses new business_licenses table

--- src/data-sources/li-business-licenses.js
+++ src/data-sources/li-business-licenses.js
@@ -6,19 +6,20 @@ export default {
     params: {
       q: function(feature){
         // if there is no eclipse_location_id, return a query that will definitely return nothing (or it will return many)
-        if (feature.properties.eclipse_location_id === null || feature.properties.eclipse_location_id === '') {
-          return "select * from business_licenses where addressobjectid in (" + null + ")";
-        }
-        var eclipseLocId = feature.properties.eclipse_location_id.split('|');
-        var str = "";
-        var i;
-        for (i = 0; i < eclipseLocId.length; i++) {
-          str += eclipseLocId[i];
-          str += ", ";
-        }
-        str = str.slice(0, str.length - 3);
+        // if (feature.properties.eclipse_location_id === null || feature.properties.eclipse_location_id === '') {
+        //   return "select * from business_licenses where addressobjectid in (" + null + ")";
+        // }
+        // var eclipseLocId = feature.properties.eclipse_location_id.split('|');
+        // var str = "";
+        // var i;
+        // for (i = 0; i < eclipseLocId.length; i++) {
+        //   str += eclipseLocId[i];
+        //   str += ", ";
+        // }
+        // str = str.slice(0, str.length - 3);
 
-        return "select * from business_licenses where addressobjectid in (" + str + ")";
+        // return "select * from business_licenses where addressobjectid in (" + str + ")";
+        return "select * from business_licenses where opa_account_num = '" + feature.properties.opa_account_num + "'";
       },
     },
   },
