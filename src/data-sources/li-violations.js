222bd4196eb75ad41375c4fbf694a332b509df90
Andy Rothwell <rothwell.andy@gmail.com>
2020-03-30 10:05:45 -0400

    changes to li-violations.js

--- src/data-sources/li-violations.js
+++ src/data-sources/li-violations.js
@@ -6,7 +6,8 @@ export default {
     params: {
       q: function(feature){
         // return "select * from li_violations where address = '" + feature.properties.street_address + "' or addresskey = '" + feature.properties.li_address_key.toString() + "'";
-        return "select * from violations where opa_account_num = '" + feature.properties.opa_account_num + "'";
+        return "select * from violations where address = '" + feature.properties.street_address + "' or addressobjectid = '" + feature.properties.li_address_key.toString() + "'";
+        // return "select * from violations where opa_account_num = '" + feature.properties.opa_account_num + "'";
       },
     },
   },
