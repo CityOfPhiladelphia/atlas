37c8f12c433097779d9c9c99b1c3a0704e96ba35
Andy Rothwell <rothwell.andy@gmail.com>
2020-04-15 15:55:52 -0400

    trying to get travis to push to s3

--- src/topics/li.js
+++ src/topics/li.js
@@ -248,14 +248,6 @@ export default {
           {
             label: 'ID',
             value: function(state, item){
-              // var eclipseLocId = state.geocode.data.properties.eclipse_location_id.split('|');
-              // var str = "" + state.geocode.data.properties.li_address_key + ",";
-              // var i;
-              // for (i = 0; i < eclipseLocId.length; i++) {
-              //   str += eclipseLocId[i];
-              //   str += ",";
-              // }
-              // str = str.slice(0, str.length - 1);
               var eclipseLocId = state.geocode.data.properties.eclipse_location_id.split('|');
               var li_address_key = state.geocode.data.properties.li_address_key.split('|');
               var j;
@@ -272,7 +264,6 @@ export default {
               str = str.slice(0, str.length - 1);
               console.log('str:', str);
               return "<a target='_blank' href='http://li.phila.gov/#details?entity=violationdetails&eid="+item.casenumber+"&key="+str+"&address="+encodeURIComponent(item.address)+"'>"+item.casenumber+" <i class='fa fa-external-link-alt'></i></a>";
-              // return "<a target='_blank' href='http://li.phila.gov/#details?entity=violationdetails&eid="+item.casenumber+"&key="+item.addressobjectid+"&address="+encodeURIComponent(item.address)+"'>"+item.casenumber+" <i class='fa fa-external-link-alt'></i></a>";
             },
           },
           {
