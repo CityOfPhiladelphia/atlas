79920652ed57237ee86fd0aff409ed15a1c810b6
Andy Rothwell <Andy.Rothwell@phila.gov>
2021-11-16 13:29:16 -0500

    changes location of zoning archive data and pdfs

--- src/data-sources/zoning-docs-eclipse.js
+++ src/data-sources/zoning-docs-eclipse.js
@@ -7,7 +7,7 @@ export default {
       q: function(feature) {
 
         if (feature.properties.eclipse_location_id === null || feature.properties.eclipse_location_id === '') {
-          return "select * from li_zoning_docs_test where address_objectid in (" + null + ")";
+          return "select * from li_zoning_docs where address_objectid in (" + null + ")";
         }
         var eclipseLocId = feature.properties.eclipse_location_id.split('|');
         var str = "'";
@@ -18,7 +18,7 @@ export default {
         }
         str = str.slice(0, str.length - 3);
 
-        return "select * from li_zoning_docs_test where address_objectid in (" + str + ")";
+        return "select * from li_zoning_docs where address_objectid in (" + str + ")";
       },
     },
   },
