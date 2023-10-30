78a726d89020955d4aa7a5edcc337e788940ad89
andy.rothwell <rothwell.andy@gmail.com>
2023-10-30 14:50:13 -0400

    testing changes for getting data by segments

--- src/data-sources/li-building-cert-summary.js
+++ src/data-sources/li-building-cert-summary.js
@@ -1,15 +1,25 @@
 export default {
   id: 'liBuildingCertSummary',
   type: 'http-get',
+  segments: true,
+  splitField: 'bin',
   url: 'https://phl.carto.com/api/v2/sql',
   deps: [ 'sources.liBuildingFootprints' ],
   options: {
     params: {
       q: function(feature, state){
-        console.log('li-building-certs, feature:', feature);
-        let bin;
-        if (feature.properties.bin && feature.properties.bin !== '') {
-          bin = feature.properties.bin.replace(/\|/g, "', '");
+        let bin = "";
+        console.log('li-building-cert-summary, feature:', feature);
+        // if (feature.properties.bin && feature.properties.bin !== '') {
+        //   bin = feature.properties.bin.replace(/\|/g, "', '");
+        if (feature.length) {
+          for (let i=0;i<feature.length;i++) {
+            bin += feature[i];
+            if (i < feature.length-1) {
+              bin += "', '";
+            }
+          }
+          console.log('after loop, bin:', bin);
         } else if (state.sources.liBuildingFootprints.data.features.length) {
           bin = state.sources.liBuildingFootprints.data.features[0].attributes.BIN;//.replace(/\|/g, "', '");
         } else {
@@ -18,6 +28,9 @@ export default {
         // console.log('li-building-cert-summary.js q function, bin:', bin, 'feature:', feature, 'state.sources.liBuildingFootprints.data.features:', state.sources.liBuildingFootprints.data.features);
         return `SELECT * FROM building_cert_summary WHERE structure_id IN ('${bin}')`;
       },
+    },
+    success: function(data) {
+      return data;
     },
   },
 };
--- src/data-sources/li-building-certs.js
+++ src/data-sources/li-building-certs.js
@@ -1,22 +1,36 @@
 export default {
   id: 'liBuildingCerts',
   type: 'http-get',
+  segments: true,
+  splitField: 'bin',
   url: 'https://phl.carto.com/api/v2/sql',
   deps: [ 'sources.liBuildingFootprints' ],
   options: {
     params: {
       q: function(feature, state) {
-        console.log('li-building-certs, feature:', feature);
-        let bin;
-        if (feature.properties.bin && feature.properties.bin !== '') {
-          bin = feature.properties.bin.replace(/\|/g, "', '");
+        let bin = "";
+        console.log('li-building-certs, feature:', feature, 'bin:', bin);
+        // if (feature.properties.bin && feature.properties.bin !== '') {
+        //   bin = feature.properties.bin.replace(/\|/g, "', '");
+        //   binSplit = bin.split(',');
+        if (feature.length) {
+          for (let i=0;i<feature.length;i++) {
+            bin += feature[i];
+            if (i < feature.length-1) {
+              bin += "', '";
+            }
+          }
+          console.log('after loop, bin:', bin);
         } else if (state.sources.liBuildingFootprints.data.features.length) {
-          bin = state.sources.liBuildingFootprints.data.features[0].attributes.BIN;//.replace(/\|/g, "', '");
+          bin = state.sources.liBuildingFootprints.data.features[0].attributes.BIN;
         } else {
           bin = '';
         }
         return `SELECT * FROM building_certs WHERE bin IN ('${bin}')`;
       },
+    },
+    success: function(data) {
+      return data;
     },
   },
 };
