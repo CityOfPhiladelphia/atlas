5fff8e9b6e1dc34de8176898fdad4993dbacbe6e
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-01-18 14:14:22 -0500

    removes redistricting

--- src/data-sources/elected-officials.js
+++ src/data-sources/elected-officials.js
@@ -9,7 +9,7 @@ export default {
         return "WITH split AS (SELECT * FROM splits WHERE precinct = '" + feature.properties.election_precinct + "') \
                 SELECT eo.*, s.ballot_file_id\
                 FROM elected_officials eo, split s \
-                WHERE eo.office = 'city_council' AND eo.district = s.city_district \
+                WHERE eo.office = 'city_council' AND eo.district = '" + feature.properties.council_district_2024 + "' \
                           OR eo.office = 'state_house' AND eo.district = s.state_house \
                           OR eo.office = 'state_senate' AND eo.district = s.state_senate \
                           OR eo.office = 'us_house' AND eo.district = s.federal_house \
