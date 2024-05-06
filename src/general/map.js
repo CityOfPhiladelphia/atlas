9a6d988789ac88591243b724fe23406036d8b061
Roland MacDavid <roland.macdavid@phila.gov>
2024-05-06 12:12:58 -0400

    use 256x256 and png8 format to hit cached tiles

--- src/general/map.js
+++ src/general/map.js
@@ -22,19 +22,19 @@ export default {
         minzoom: 0,
         maxzoom: 22,
       },
-      // https://gis-svc.databridge.phila.gov/arcgis/rest/services/Atlas/ZoningMap/MapServer/export?dpi=110\
       source: {
         tiles: [
-          'https://citygeo-geoserver.databridge.phila.gov/geoserver/wms?service=WMS&version=1.1.0&request=GetMap&layers=atlas_zoning_grouped&bbox={bbox-epsg-3857}&width=1024&height=1024&srs=EPSG%3A3857&styles=&format=image/png&transparent=true',
-          // 'https://citygeo-geocoder-pub.databridge.phila.gov/arcgis/rest/services/Atlas/ZoningMap/MapServer/export?dpi=120\
-          //   &transparent=true\
-          //   &format=png32\
-          //   &bbox={bbox-epsg-3857}\
-          //   &bboxSR=3857\
-          //   &imageSR=3857\
-          //   &size=512,512\
-          //   &f=image\
-          // ',
+          'https://citygeo-geoserver.databridge.phila.gov/geoserver/wms?service=WMS\
+            &version=1.3.0\
+            &request=GetMap\
+            &layers=atlas_zoning_grouped\
+            &bbox={bbox-epsg-3857}\
+            &width=256\
+            &height=256\
+            &srs=EPSG%3A3857\
+            &format=image/png8\
+            &transparent=true\
+          ',
         ],
         type: 'raster',
         tileSize: 1024,
